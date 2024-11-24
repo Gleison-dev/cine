1cd8435d48169498c63ebcdeec132933246cd0f0
Gleison <gleisonrgomes@gmail.com>
2024-11-24 19:42:29 -0300

    feat: image slider file created and imported on home pae

---
+++ src/components/ImageSlider/index.jsx
@@ -0,0 +1,7 @@
+export default function ImageSlider({ bg_img, title, genre, year, synopsis, trailer, watch }) {
+  return (
+    <>
+      <section></section>
+    </>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import Header from "../../components/Header";
+import ImageSlider from "../../components/ImageSlider";
 
 export default function Home() {
   return (
     <>
       <section>
         <Header />
+      </section>
+      <section>
+        <ImageSlider />
       </section>
     </>
   );
