3e9d9f3663f2333a45ec5e55a70e2256d730f78b
Gleison <gleisonrgomes@gmail.com>
2024-11-24 18:57:05 -0300

    feat: search icon imported on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/cineafro.png";
+import search_icon from "../../assets/search_icon.svg";
 import Li from "../Li";
 
 export default function Header() {
@@ -6,15 +7,18 @@ export default function Header() {
     <>
       <section>
         <div>
-            <img src={logo} alt="logo do cineafro" />
+          <img src={logo} alt="logo do cineafro" />
         </div>
         <div>
-            <ul>
-                <Li text="Home"/>
-                <Li text="Filmes"/>
-                <Li text="Sobre"/>
-            </ul>
-            <input type="text" placeholder="Pesquisar filme"/>
+          <ul>
+            <Li text="Home" />
+            <Li text="Filmes" />
+            <Li text="Sobre" />
+          </ul>
+          <input type="text" placeholder="Pesquisar filme" />
+          <button>
+            <img src={search_icon} alt="Ícone de pesquisa" />
+          </button>
         </div>
       </section>
     </>
