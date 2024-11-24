413a5517f9e6ad302a58796933edb78f1bc83941
Gleison <gleisonrgomes@gmail.com>
2024-11-24 19:47:07 -0300

    feat: structure of image slider created

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -1,7 +1,42 @@
-export default function ImageSlider({ bg_img, title, genre, year, synopsis, trailer, watch }) {
+export default function ImageSlider({
+  bg_img,
+  alt,
+  title,
+  genre,
+  year,
+  synopsis,
+  trailer,
+  watch,
+  text_trailer,
+  text_watch,
+}) {
   return (
     <>
-      <section></section>
+      <section>
+        <div>
+          <img src={bg_img} alt={alt} />
+        </div>
+        <div>
+          <div>
+            <h1>{title}</h1>
+          </div>
+          <div>
+            <p>{genre}</p>
+            <p>{year}</p>
+          </div>
+          <div>
+            <p>{synopsis}</p>
+          </div>
+          <div>
+            <a href={trailer}>
+              <button>{text_trailer}</button>
+            </a>
+            <a href={watch}>
+              <button>{text_watch}</button>
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
