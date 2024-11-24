e405283f5435ab3d8ff75a7f190299aad8a6ffad
Gleison <gleisonrgomes@gmail.com>
2024-11-24 18:39:44 -0300

    feat: li component created and imported on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/cineafro.png";
+import Li from "../Li";
 
 export default function Header() {
   return (
@@ -6,6 +7,11 @@ export default function Header() {
       <section>
         <div>
             <img src={logo} alt="logo do cineafro" />
+        </div>
+        <div>
+            <ul>
+                <Li />
+            </ul>
         </div>
       </section>
     </>
