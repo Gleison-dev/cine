6e49d52ebe3cd0ea9a17df636b19b52d690a09b5
Gleison <gleisonrgomes@gmail.com>
2024-11-24 18:35:44 -0300

    feat: header file created and imported on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import Header from "../../components/Header";
+
 export default function Home() {
   return (
     <>
-      <section></section>
+      <section>
+        <Header />
+      </section>
     </>
   );
 }
