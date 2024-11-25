89e92790c733c64930af5f5e92601a92f9e87df4
Gleison <gleisonrgomes@gmail.com>
2024-11-25 19:28:58 -0300

    feat: adjusting style of image slider component

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -11,40 +11,49 @@ export default function ImageSlider({
       <section className="text-white">
         <div className="space-y-8">
           <div>
-            <h1>{title}</h1>
+            <h1 className="text-6xl font-bold">{title}</h1>
           </div>
-          <div className="flex gap-5">
+          <div className="flex gap-5 text-lg">
             <p>{genre}</p>
             <p>{year}</p>
           </div>
           <div className="w-96">
-            <p>{synopsis}</p>
+            <p className="text-xl">{synopsis}</p>
           </div>
           <div className="flex gap-10">
             <a href={trailer}>
-              <button>TRAILER</button>
+              <button className="w-36 h-14 rounded-md text-black bg-dark_orange hover:bg-light_orange">
+                TRAILER
+              </button>
             </a>
             <a href={watch}>
-              <button>ASSISTIR</button>
+              <button
+                className="w-36 h-14 rounded-md text-black bg-dark_orange hover:bg-light_orange"
+                hover:text-black
+              >
+                ASSISTIR
+              </button>
             </a>
           </div>
         </div>
-        <div className="flex justify-center gap-5">
+        <div className="flex justify-center mt-10 mr-16 gap-24">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
+            width="50"
+            height="50"
             fill="#000000"
             viewBox="0 0 256 256"
+            className="fill-current hover:text-dark_orange"
           >
             <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H107.31l18.35,18.34a8,8,0,0,1-11.32,11.32l-32-32a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,11.32L107.31,120H168A8,8,0,0,1,176,128Z"></path>
           </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
+            width="50"
+            height="50"
             fill="#000000"
             viewBox="0 0 256 256"
+            className="fill-current hover:text-dark_orange"
           >
             <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm45.66-93.66a8,8,0,0,1,0,11.32l-32,32a8,8,0,0,1-11.32-11.32L148.69,136H88a8,8,0,0,1,0-16h60.69l-18.35-18.34a8,8,0,0,1,11.32-11.32Z"></path>
           </svg>
