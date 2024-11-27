04b39390ed7797b6bbc7aa8d503272a13a1ff4b6
Gleison <gleisonrgomes@gmail.com>
2024-11-27 19:35:18 -0300

    feat: image slider function created

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -1,4 +1,5 @@
 export default function ImageSlider({
+  image,
   title,
   genre,
   year,
@@ -59,28 +60,6 @@ export default function ImageSlider({
               </button>
             </a>
           </div>
-        </div>
-        <div className="flex justify-center mt-10 mr-16 gap-24">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
-            fill="#000000"
-            viewBox="0 0 256 256"
-            className="fill-current hover:text-dark_orange hover:cursor-pointer"
-          >
-            <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H107.31l18.35,18.34a8,8,0,0,1-11.32,11.32l-32-32a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,11.32L107.31,120H168A8,8,0,0,1,176,128Z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
-            fill="#000000"
-            viewBox="0 0 256 256"
-            className="fill-current hover:text-dark_orange hover:cursor-pointer"
-          >
-            <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm45.66-93.66a8,8,0,0,1,0,11.32l-32,32a8,8,0,0,1-11.32-11.32L148.69,136H88a8,8,0,0,1,0-16h60.69l-18.35-18.34a8,8,0,0,1,11.32-11.32Z"></path>
-          </svg>
         </div>
       </section>
     </>
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,24 +1,65 @@
+import { useEffect, useState } from "react";
 import ImageSlider from "../ImageSlider";
-import "./style.css";
+import black_panther from "../../assets/black_panther_banner.jpg";
+import years_a_slave from "../../assets/years_a_slave_banner.jpg";
+
+const images = [
+  {
+    alt: "Imagem do Pantera Negra",
+    title: "Pantera Negra",
+    genre: "Aventura",
+    year: "2018",
+    synopsis:
+      "Lorem ipsum dolor sit amet. Aut sunt aliquam et dolores velit est ipsa  voluptatem ut impedit quisquam! Sit ratione vitae ut galisum accusamus  At fugit praesentium? Est labore maxime eum consectetur neque ex  voluptas quia qui dicta illo et similique rerum 33 modi aperiam. Non  quod quisquam ut quos temporibus est voluptatem quaerat ut vitae  quisquam.",
+    trailer: "#",
+    watch: "#",
+    background: black_panther,
+  },
+  {
+    alt: "Imagem de 12 Anos de Escravidão",
+    title: "12 Anos de Escravidão",
+    genre: "Drama",
+    year: "2013",
+    synopsis:
+      "Lorem ipsum dolor sit amet. Aut sunt aliquam et dolores velit est ipsa  voluptatem ut impedit quisquam! Sit ratione vitae ut galisum accusamus  At fugit praesentium? Est labore maxime eum consectetur neque ex  voluptas quia qui dicta illo et similique rerum 33 modi aperiam. Non  quod quisquam ut quos temporibus est voluptatem quaerat ut vitae  quisquam.",
+    trailer: "#",
+    watch: "#",
+    background: years_a_slave,
+  },
+];
 
 export default function Main() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 7000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <section className="relative">
-        <div className="bg h-screen opacity-25 absolute inset-0"></div>
+        <div
+          className="bg h-screen opacity-25 absolute inset-0"
+          style={{
+            backgroundImage: `url(${images[currentIndex].background})`, // Adicione a URL corretamente
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
         <div className="flex h-screen ml-52 items-center relative z-10">
           <ImageSlider
-            alt="Imagem do pantera negra"
-            title="Pantera Negra"
-            genre="Aventura"
-            year="2018"
-            synopsis="Lorem ipsum dolor sit amet. Aut sunt aliquam et dolores velit est ipsa 
-voluptatem ut impedit quisquam! Sit ratione vitae ut galisum accusamus 
-At fugit praesentium? Est labore maxime eum consectetur neque ex  voluptas quia
-qui dicta illo et similique rerum 33 modi aperiam. Non  quod quisquam ut quos temporibus
-est voluptatem quaerat ut vitae  quisquam."
-            trailer="#"
-            watch="#"
+            image={images[currentIndex].background}
+            alt={images[currentIndex].alt}
+            title={images[currentIndex].title}
+            genre={images[currentIndex].genre}
+            year={images[currentIndex].year}
+            synopsis={images[currentIndex].synopsis}
+            trailer={images[currentIndex].trailer}
+            watch={images[currentIndex].watch}
           />
         </div>
       </section>
