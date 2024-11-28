6df0682f0128f4b662dc16b5051c8a4d8d54781f
Gleison <gleisonrgomes@gmail.com>
2024-11-28 15:32:29 -0300

    feat: importing list genre on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header";
+import ListGenre from "../../components/ListGenre";
 import Main from "../../components/Main";
 
 export default function Home() {
@@ -9,6 +10,9 @@ export default function Home() {
       </section>
       <section>
         <Main />
+      </section>
+      <section>
+        <ListGenre />
       </section>
     </>
   );
