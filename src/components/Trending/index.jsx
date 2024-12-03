63a1335c0443372dc0d0e2b3b24039aa9b6b2f05
Gleison <gleisonrgomes@gmail.com>
2024-12-03 17:55:18 -0300

    feat: treding card imported on trending

--- src/components/Trending/index.jsx
+++ src/components/Trending/index.jsx
@@ -1,9 +1,14 @@
+import TrendingCard from "../TrendingCard";
+
 export default function Trending() {
   return (
     <>
       <section>
         <div>
             <h1>Em alta</h1>
+        </div>
+        <div>
+          <TrendingCard />
         </div>
       </section>
     </>
