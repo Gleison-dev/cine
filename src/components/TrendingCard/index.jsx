cf21244f78a726eb0479e2a4aabc9a40ccf95727
Gleison <gleisonrgomes@gmail.com>
2024-12-03 18:04:49 -0300

    feat: styling trending card component

--- src/components/TrendingCard/index.jsx
+++ src/components/TrendingCard/index.jsx
@@ -1,7 +1,20 @@
-export default function TrendingCard() {
+import Button from "../Button";
+
+export default function TrendingCard({ image, alt, synopsis }) {
   return (
     <>
-      <section></section>
+      <section className="flex justify-center items-center gap-10 border">
+        <div>
+          <img className="w-52" src={image} alt={alt} />
+        </div>
+        <div className="flex flex-col w-80 space-y-8 items">
+          <p>{synopsis}</p>
+          <div className="space-x-8">
+            <Button text="TRAILER" />
+            <Button text="ASSISTIR" />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
