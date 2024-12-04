b05c16beef076820a1d718fc4031e78c00ec690f
Gleison <gleisonrgomes@gmail.com>
2024-12-04 09:25:31 -0300

    feat: link to trailer and streaming added

--- src/components/TrendingCard/index.jsx
+++ src/components/TrendingCard/index.jsx
@@ -1,7 +1,7 @@
 import Button from "../Button";
 import "./style.css";
 
-export default function TrendingCard({ image, alt, synopsis }) {
+export default function TrendingCard({ image, alt, synopsis, link_trailer, link_watch }) {
   return (
     <>
       <section className="section-card flex justify-center items-center gap-10 border p-4 rounded-md">
@@ -11,8 +11,12 @@ export default function TrendingCard({ image, alt, synopsis }) {
         <div className="flex flex-col w-80 space-y-8 items">
           <p>{synopsis}</p>
           <div className="space-x-8">
-            <Button text="TRAILER" />
-            <Button text="ASSISTIR" />
+            <a href={link_trailer} target="_blank">
+              <Button text="TRAILER" />
+            </a>
+            <a href={link_watch} target="_blank">
+              <Button text="ASSISTIR" />
+            </a>
           </div>
         </div>
       </section>
