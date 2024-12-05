fb58cd24dfa09c076ca01c60450f2aa73874e35f
Gleison <gleisonrgomes@gmail.com>
2024-12-05 10:00:33 -0300

    feat: color of card aded

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        "body": "#474448",
-        "dark_orange": "#EABE7C",
-        "light_orange": "#ECE4B7",
-        "body_white": "#E6EBE0"
-      }
+        body: "#474448",
+        dark_orange: "#EABE7C",
+        light_orange: "#ECE4B7",
+        body_white: "#E6EBE0",
+        black_card: "#171A21",
+      },
     },
   },
   plugins: [],
