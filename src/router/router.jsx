882ab4763f241f149eb29e6e9546241d2a7cb28e
Gleison <gleisonrgomes@gmail.com>
2024-12-06 13:12:15 -0300

    feat router to about page created

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/index";
+import About from "../pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/about",
+    element: <About />
+  }
 ]);
 
 export { router };
