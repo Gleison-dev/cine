9bca97c63ebc6c30a24b4404604302623b98f5f4
Gleison <gleisonrgomes@gmail.com>
2024-12-07 14:53:30 -0300

    feat: Li component stylezed

--- src/components/Li/index.jsx
+++ src/components/Li/index.jsx
@@ -1,7 +1,7 @@
 export default function Li({ text }) {
   return (
     <>
-      <li className="text-lg mr-20 hover:text-dark_orange cursor-pointer">{text}</li>
+      <li className="text-xl mr-20 hover:text-dark_orange cursor-pointer">{text}</li>
     </>
   );
 }
