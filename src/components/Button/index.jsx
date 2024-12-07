197cc22efa818ffdb5185c6e01d1252804463715
Gleison <gleisonrgomes@gmail.com>
2024-12-06 21:26:05 -0300

    feat: onClick added

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,10 @@
-export default function Button({ text }) {
+export default function Button({ text, onClick }) {
   return (
     <>
-      <button className="w-36 h-14 rounded-md text-black bg-dark_orange hover:bg-light_orange">
+      <button
+        className="w-36 h-14 rounded-md text-black bg-dark_orange hover:bg-light_orange"
+        onClick={onClick}
+      >
         {text}
       </button>
     </>
