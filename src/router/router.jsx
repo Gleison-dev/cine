f0e026aa8df195063f08fb1f565931508fcb8331
Gleison <gleisonrgomes@gmail.com>
2024-12-07 23:18:51 -0300

    feat: router of get all movies created

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/index";
 import About from "../pages/About";
 import RegisterMovie from "../pages/RegisterMovie";
+import GetAllMovies from "../pages/GetAllMovies";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/movies",
+    element: <GetAllMovies />,
   },
   {
     path: "/about",
