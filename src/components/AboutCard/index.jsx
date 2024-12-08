5efda1324a2d016545f238e9e7b0f183d476ab81
Gleison <gleisonrgomes@gmail.com>
2024-12-07 21:54:52 -0300

    feat: shadow added

--- src/components/AboutCard/index.jsx
+++ src/components/AboutCard/index.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 export default function AboutCard({ title, text }) {
   return (
     <>
-      <section className="section-main space-y-10 bg-light_orange p-6 rounded-md">
+      <section className="section-main space-y-10 bg-light_orange p-6 rounded-md shadow-2xl">
         <div>
           <h1 className="text-3xl text-black_card">
             <strong>{title}</strong>
