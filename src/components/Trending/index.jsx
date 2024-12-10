66379183196b7e5521d77fe6485a071c46b92c58
Gleison <gleisonrgomes@gmail.com>
2024-12-10 08:57:52 -0300

    feat: new movies on trending added

--- src/components/Trending/index.jsx
+++ src/components/Trending/index.jsx
@@ -23,6 +23,21 @@ export default function Trending() {
             link_watch="https://www.netflix.com/title/81557731"
           />
         </div>
+        <div className="flex justify-evenly gap-10 mt-16">
+          <TrendingCard
+            image="https://upload.wikimedia.org/wikipedia/pt/1/1b/%C3%93_Pa%C3%AD%2C_%C3%93_2.png"
+            synopsis="
+Dona Joana vive o luto pela perda de Cosme e Damião e segue enfrentando os problemas de convivência com os inquilinos do cortiço no bairro do Pelourinho. O local se prepara para a festa de Iemanjá, enquanto lida com as polêmicas dos vizinhos."
+            link_trailer="https://youtu.be/2j8UPVfGy-E?si=3GdZzGez_XyFrZsF"
+            link_watch="https://globoplay.globo.com/o-pai-o-2/t/q5CfFTw1Zz/"
+          />
+          <TrendingCard
+            image="https://www.sonypictures.com.br/sites/brazil/files/2023-08/SN_HomemAranha_1400x2100%20%281%29.jpg"
+            synopsis="Depois de se reunir com Gwen Stacy, Homem-Aranha é jogado no multiverso. Lá, o super-herói aracnídeo encontra uma numerosa equipe encarregada de proteger sua própria existência."
+            link_trailer="https://youtu.be/ME0QiRVsmVI?si=eeZ05jOlO4fw_Tqh"
+            link_watch="https://www.max.com/br/pt/movies/homem-aranha-atraves-do-aranhaverso/08f3260f-1512-4c90-a3df-fc2e407ba708?utm_source=universal_search"
+          />
+        </div>
       </section>
     </>
   );
