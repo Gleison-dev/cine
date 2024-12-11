c0dd26f0fddeaf4e6a1f66053bef2b2ab7fe2b65
Gleison <gleisonrgomes@gmail.com>
2024-12-11 10:20:54 -0300

    feat: testing card movie

--- src/pages/GetAllMovies/index.jsx
+++ src/pages/GetAllMovies/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import axios from "axios";
+import CardMovie from "../../components/CardMovie";
 
 export default function GetAllMovies() {
   const [movies, setMovies] = useState([]);
@@ -30,7 +31,15 @@ export default function GetAllMovies() {
           <div>
             <h1>Filmes</h1>
           </div>
-          <div></div>
+          <div>
+            <CardMovie
+              title="Pantera Negra"
+              image="https://upload.wikimedia.org/wikipedia/pt/thumb/2/28/Black_Panther_2018.jpg/250px-Black_Panther_2018.jpg"
+              genre="Ação"
+              year="2018"
+              synopsis="Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados."
+            />
+          </div>
         </div>
       </section>
     </>
