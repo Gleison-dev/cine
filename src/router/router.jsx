e99558095f50beba3dff60b86bab118d5711a2b5
Gleison <gleisonrgomes@gmail.com>
2024-12-12 09:19:20 -0300

    feat: new routers to genre pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,14 @@ import Home from "../pages/Home/index";
 import About from "../pages/About";
 import RegisterMovie from "../pages/RegisterMovie";
 import GetAllMovies from "../pages/GetAllMovies";
+import ActionGenre from "../pages/ActionGenre";
+import AdventureGenre from "../pages/AdventureGenre";
+import ComedyGenre from "../pages/ComedyGenre";
+import DramaGenre from "../pages/DramaGenre";
+import DocumentaryGenre from "../pages/DocumentaryGenre";
+import SuspenseGenre from "../pages/SuspenseGenre";
+import RomanceGenre from "../pages/RomanceGenre";
+import AnimationGenre from "../pages/AnimationGenre";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +28,38 @@ const router = createBrowserRouter([
   {
     path: "/registerMovie",
     element: <RegisterMovie />,
+  },
+  {
+    path: "/actionGenre",
+    element: <ActionGenre />,
+  },
+  {
+    path: "/adventureGenre",
+    element: <AdventureGenre />,
+  },
+  {
+    path: "/comedyGenre",
+    element: <ComedyGenre />,
+  },
+  {
+    path: "/dramaGenre",
+    element: <DramaGenre />,
+  },
+  {
+    path: "/documentaryGenre",
+    element: <DocumentaryGenre />,
+  },
+  {
+    path: "/suspenseGenre",
+    element: <SuspenseGenre />,
+  },
+  {
+    path: "/romanceGenre",
+    element: <RomanceGenre />,
+  },
+  {
+    path: "/animationGenre",
+    element: <AnimationGenre />,
   },
 ]);
 
