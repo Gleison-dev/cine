5923fb95f01bb56cda12692756d06c75797c3365
Gleison <gleisonrgomes@gmail.com>
2024-12-12 08:30:18 -0300

    feat: state of loading added

--- src/pages/GetAllMovies/index.jsx
+++ src/pages/GetAllMovies/index.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import axios from "axios";
 import CardMovie from "../../components/CardMovie";
+import "./style.css";
 
 export default function GetAllMovies() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getAllMovies = async () => {
     try {
@@ -12,8 +14,10 @@ export default function GetAllMovies() {
         "https://cine-afro-backend.onrender.com/movies"
       );
       setMovies(data.movies);
+      setIsLoading(false);
     } catch (error) {
       return console.error(error);
+      setIsLoading(false);
     }
   };
 
@@ -31,21 +35,25 @@ export default function GetAllMovies() {
           <div className="flex justify-start ml-40">
             <h1 className="text-dark_orange text-3xl underline">Filmes</h1>
           </div>
-          <div className="flex flex-wrap justify-evenly gap-8 mt-20">
-            {Array.isArray(movies) &&
-              movies.map((i) => (
-                <CardMovie
-                  key={i.id}
-                  title={i.title}
-                  genre={i.genre}
-                  year_release={i.year_release}
-                  image={i.url_image}
-                  synopsis={i.synopsis}
-                  link_trailer={i.trailer}
-                  link_watch={i.streaming}
-                />
-              ))}
-          </div>
+          {isLoading ? (
+            <div className="loader"></div>
+          ) : (
+            <div className="flex flex-wrap justify-evenly gap-8 mt-20">
+              {Array.isArray(movies) &&
+                movies.map((i) => (
+                  <CardMovie
+                    key={i.id}
+                    title={i.title}
+                    genre={i.genre}
+                    year_release={i.year_release}
+                    image={i.url_image}
+                    synopsis={i.synopsis}
+                    link_trailer={i.trailer}
+                    link_watch={i.streaming}
+                  />
+                ))}
+            </div>
+          )}
         </div>
       </section>
     </>
