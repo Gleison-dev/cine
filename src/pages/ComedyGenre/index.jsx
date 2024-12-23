ec721bba53297328a049118bb1f73ba0fb299439
Gleison <gleisonrgomes@gmail.com>
2024-12-23 20:06:28 -0300

    feat: comedy genre created

--- src/pages/ComedyGenre/index.jsx
+++ src/pages/ComedyGenre/index.jsx
@@ -1,7 +1,13 @@
+import MovieByGenre from "../../components/MovieByGenre/index";
+
 export default function ComedyGenre() {
   return (
     <>
-      <section></section>
+      <section>
+        <div>
+          <MovieByGenre url_genre="Comédia" text_genre="Comédia" />
+        </div>
+      </section>
     </>
   );
 }
