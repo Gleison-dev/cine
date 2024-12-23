ef674600f95a5795d73c5ba499b8b16bb1606a6c
Gleison <gleisonrgomes@gmail.com>
2024-12-23 20:13:58 -0300

    feat: romance genre created

--- src/pages/RomanceGenre/index.jsx
+++ src/pages/RomanceGenre/index.jsx
@@ -1,7 +1,13 @@
+import MovieByGenre from "../../components/MovieByGenre/index";
+
 export default function RomanceGenre() {
   return (
     <>
-      <section></section>
+      <section>
+        <div>
+          <MovieByGenre url_genre="Romance" text_genre="Romance" />
+        </div>
+      </section>
     </>
   );
 }
