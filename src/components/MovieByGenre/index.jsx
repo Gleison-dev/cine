97fc2b807c0ed747dc14e2d733128c12291493b7
Gleison <gleisonrgomes@gmail.com>
2024-12-23 19:45:50 -0300

    faet: fucntion adjusted

--- src/components/MovieByGenre/index.jsx
+++ src/components/MovieByGenre/index.jsx
@@ -1,34 +1,32 @@
 import axios from "axios";
 import Header from "../Header/index";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./style.css";
 import CardMovie from "../CardMovie/index";
 
-export default function MovieByGenre({
-  url_genre,
-  text_genre,
-  title,
-  genre,
-  year_release,
-  image,
-  alt,
-  synopsis,
-  link_trailer,
-  link_watch,
-}) {
+export default function MovieByGenre({ url_genre, text_genre }) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const handleFunction = async () => {
-    const { data } = await axios.get(
-      `https://cine-afro-backend.onrender.com/${url_genre}`
-    );
-    setMovies(data.MovieByGenre);
-    setIsLoading(false);
+    try {
+      const { data } = await axios.get(
+        `https://cine-afro-backend.onrender.com/movieByGenre?genre=${url_genre}`
+      );
+      setMovies(data.movieByGenre);
+      setIsLoading(false);
+      console.log(movies);
+    } catch (error) {
+      return console.error("Não foi possível buscar os livros: ", error);
+    }
   };
+
+  useEffect(() => {
+    handleFunction();
+  }, []);
   return (
     <>
-      <section>
+      <section className="text-body_white">
         <div>
           <Header />
         </div>
@@ -44,17 +42,20 @@ export default function MovieByGenre({
           </div>
         ) : (
           <div>
-            <CardMovie
-              key={key}
-              title={title}
-              genre={genre}
-              year_release={year_release}
-              image={image}
-              alt={alt}
-              synopsis={synopsis}
-              link_trailer={link_trailer}
-              link_watch={link_trailer}
-            />
+            {Array.isArray(movies) &&
+              movies.map((i) => (
+                <CardMovie
+                  key={i.id}
+                  title={i.title}
+                  genre={i.genre}
+                  year_release={i.year_release}
+                  image={i.url_image}
+                  alt={i.alt}
+                  synopsis={i.synopsis}
+                  link_trailer={i.trailer}
+                  link_watch={i.streaming}
+                />
+              ))}
           </div>
         )}
       </section>
