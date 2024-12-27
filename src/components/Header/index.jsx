27a44724fab124b946811f9ca8657ed6e6c9a1e7
Gleison <gleisonrgomes@gmail.com>
2024-12-27 10:42:56 -0300

    feat: link to get movie by title page created

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,23 +23,20 @@ export default function Header() {
           </ul>
         </div>
         <div className="flex items-center">
-          <input
-            className="w-72 bg-body mr-5 outline-none border p-2 text-dark_orange rounded-md"
-            type="text"
-            placeholder="Pesquisar filme"
-          />
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="#000000"
-              viewBox="0 0 256 256"
-              className="fill-current hover:text-dark_orange"
-            >
-              <path d="M228.24,219.76l-51.38-51.38a86.15,86.15,0,1,0-8.48,8.48l51.38,51.38a6,6,0,0,0,8.48-8.48ZM38,112a74,74,0,1,1,74,74A74.09,74.09,0,0,1,38,112Z"></path>
-            </svg>
-          </button>
+          <Link to="/getMovieByTitle">
+            <button className="flex items-center gap-2 text-lg bg-dark_orange p-4 rounded-md text-black">
+              Pesquisar filme
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                fill="#000000"
+                viewBox="0 0 256 256"
+              >
+                <path d="M228.24,219.76l-51.38-51.38a86.15,86.15,0,1,0-8.48,8.48l51.38,51.38a6,6,0,0,0,8.48-8.48ZM38,112a74,74,0,1,1,74,74A74.09,74.09,0,0,1,38,112Z"></path>
+              </svg>
+            </button>
+          </Link>
         </div>
       </section>
       <hr className="border-2 border-dark_orange" />
